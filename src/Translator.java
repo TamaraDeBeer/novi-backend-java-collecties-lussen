edb15f197affb1a2f2a7e5edd472ea9f1cb39ab2
TamaraDeBeer <tamara.debeer@hotmail.com>
2023-05-31 12:46:33 +0200

    huiswerkopdracht af

---
+++ src/Translator.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Translator {
+    Map<Integer, String> numericAlpha = new HashMap<>();
+
+
+public Translator(String[] alphabetic, Integer[] numeric) {
+    for(int i = 0; i < alphabetic.length; i++); {
+        numericAlpha.put(numeric[1], alphabetic[1]);
+    }
+}
+    public String translate(Integer number) {
+        return numericAlpha.get(number);
+    }
+}
